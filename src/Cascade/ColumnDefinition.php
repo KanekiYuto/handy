b4d4d598510c653ce42734ea415ed6b7cb289575
KanekiYuto <kaneki.yuto.404@gmail.com>
2024-12-05 12:48:06 +0800

    重构Cascade

--- src/Cascade/ColumnDefinition.php
+++ src/Cascade/ColumnDefinition.php
@@ -3,12 +3,16 @@
 namespace KanekiYuto\Handy\Cascade;
 
 use Closure;
+use Illuminate\Contracts\Database\Query\Expression;
 use KanekiYuto\Handy\Cascade\Params\Column as ColumnParams;
+use KanekiYuto\Handy\Cascade\Trait\Laravel\ColumnDefinition as LaravelColumnDefinition;
 
 class ColumnDefinition
 {
 
-	private ColumnParams $columnParams;
+	use LaravelColumnDefinition;
+
+	protected ColumnParams $columnParams;
 
 	public function __construct(ColumnParams $columnParams)
 	{
@@ -18,39 +22,266 @@ class ColumnDefinition
 	/**
 	 * 与 Laravel ColumnDefinition 保持一致
 	 *
-	 * @param bool $value
+	 * @param  bool  $value
 	 *
-	 * @return LaravelColumnDefinition|ColumnDefinition
+	 * @return ColumnDefinition
 	 */
-	public function nullable(bool $value = true): self
+	public function nullable(bool $value = true): static
 	{
-		$params = (object)[];
-
-		if ($value !== true) {
-			$params->value = $value;
-		}
-
-		$this->columnParams->setMigrationParam(__FUNCTION__, $params);
-
-		return $this;
+		return $this->autoParams(__FUNCTION__, [
+			'$value' => $value,
+		], $this);
 	}
 
 	/**
 	 * 与 Laravel ColumnDefinition 保持一致
 	 *
-	 * @param string $comment
+	 * @param  string  $comment
 	 *
-	 * @return LaravelColumnDefinition|ColumnDefinition
+	 * @return ColumnDefinition
 	 */
-	public function comment(string $comment): self
+	public function comment(string $comment): static
 	{
 		$this->columnParams->setComment($comment);
 
-		$this->columnParams->setMigrationParam(__FUNCTION__, (object)[
-			'comment' => $comment
-		]);
+		return $this->autoParams(__FUNCTION__, [
+			'$comment' => $comment,
+		], $this);
+	}
 
-		return $this;
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @param  bool  $value
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function primary(bool $value = true): static
+	{
+		return $this->autoParams(__FUNCTION__, [
+			'$value' => $value,
+		], $this);
+	}
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @param  bool|string|null  $indexName
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function unique(bool|string|null $indexName = null): self
+	{
+		return $this->autoParams(__FUNCTION__, [
+			'$indexName' => $indexName,
+		], $this);
+	}
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @param  bool|string|null  $indexName
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function index(bool|string|null $indexName = null): self
+	{
+		return $this->autoParams(__FUNCTION__, [
+			'$indexName' => $indexName,
+		], $this);
+	}
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @param  string  $column
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function after(string $column): self
+	{
+		return $this->autoParams(__FUNCTION__, [
+			'$column' => $column,
+		], $this);
+	}
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @param  string|Expression  $expression
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function storedAs(string|Expression $expression): self
+	{
+		return $this->autoParams(__FUNCTION__, [
+			'$expression' => $expression,
+		], $this);
+	}
+
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @param  mixed  $value
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function default(mixed $value): self
+	{
+		return $this->autoParams(__FUNCTION__, [
+			'$value' => $value,
+		], $this);
+	}
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @param  int  $startingValue
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function from(int $startingValue): self
+	{
+		return $this->autoParams(__FUNCTION__, [
+			'$startingValue' => $startingValue,
+		], $this);
+	}
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @param  string  $charset
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function charset(string $charset): self
+	{
+		return $this->autoParams(__FUNCTION__, [
+			'$charset' => $charset,
+		], $this);
+	}
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @param  string|Expression  $expression
+	 *
+	 * @return LaravelColumnDefinition|ColumnDefinition
+	 */
+	public function virtualAs(string|Expression $expression): self
+	{
+		return $this->autoParams(__FUNCTION__, [
+			'$expression' => $expression,
+		], $this);
+	}
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @param  string|Expression|null  $expression
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function generatedAs(string|Expression $expression = null): self
+	{
+		return $this->autoParams(__FUNCTION__, [
+			'$expression' => $expression,
+		], $this);
+	}
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function unsigned(): self
+	{
+		return $this->autoParams(__FUNCTION__, [], $this);
+	}
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function useCurrent(): self
+	{
+		return $this->autoParams(__FUNCTION__, [], $this);
+	}
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function useCurrentOnUpdate(): self
+	{
+		return $this->autoParams(__FUNCTION__, [], $this);
+	}
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function always(): self
+	{
+		return $this->autoParams(__FUNCTION__, [], $this);
+	}
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function first(): self
+	{
+		return $this->autoParams(__FUNCTION__, [], $this);
+	}
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function invisible(): self
+	{
+		return $this->autoParams(__FUNCTION__, [], $this);
+	}
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @param  string  $collation
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function collation(string $collation): self
+	{
+		return $this->autoParams(__FUNCTION__, [
+			'$collation' => $collation,
+		], $this);
+	}
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function autoIncrement(): self
+	{
+		return $this->autoParams(__FUNCTION__, [], $this);
+	}
+
+	/**
+	 * 与 Laravel ColumnDefinition 保持一致
+	 *
+	 * @return ColumnDefinition
+	 */
+	public function change(): self
+	{
+		return $this->autoParams(__FUNCTION__, [], $this);
 	}
 
 	/**
--- src/Cascade/Trait/Laravel/Helper.php
+++ src/Cascade/Trait/Laravel/Helper.php
@@ -1,29 +1,30 @@
 <?php
 
-namespace KanekiYuto\Handy\Cascade\Trait;
+namespace KanekiYuto\Handy\Cascade\Trait\Laravel;
 
 use stdClass;
 use ReflectionMethod;
 use ReflectionException;
 use function Laravel\Prompts\error;
 
-trait Migration
+trait Helper
 {
 
 	/**
 	 * 自动判断是否需要使用方法参数 [如果值不是默认的话]
 	 *
+	 * @param  string  $class
 	 * @param  string  $fn
 	 * @param  array   $params
 	 *
 	 * @return stdClass
 	 */
-	protected function useParams(string $fn, array $params): stdClass
+	protected function useParams(string $class, string $fn, array $params): stdClass
 	{
 		$validParams = [];
 
 		try {
-			$method = new ReflectionMethod(__CLASS__, $fn);
+			$method = new ReflectionMethod($class, $fn);
 
 			foreach ($method->getParameters() as $param) {
 				$paramName = $param->getName();
